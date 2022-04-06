107b180a4d58130ba56dccc36e6e1f6b9e77d141
Goncroz <86432569+Goncroz@users.noreply.github.com>
2022-04-06 14:19:22 -0300

    OCP na prática

--- 0x01/ocp/Main.java
+++ 0x01/ocp/Main.java
@@ -1,21 +1,15 @@
-package vehicles;
-
-import java.lang.reflect.TypeVariable;
 
 public class Main {
-	private static TypeVehicle typeVehicle;
-
-	public static void main(String[] args) {
-		typeVehicle = TypeVehicle.CAR;
+    private static TypeVehicle typeVehicle;
+    public static void main(String[] args){
+        typeVehicle = TypeVehicle.CAR;
 
         if (typeVehicle == TypeVehicle.CAR){
-            Vehicle vehicle = new Vehicle("Azul", "2022", 2.6);
+            Vehicle vehicle = new Vehicle("white", "2022", 2.0);
             vehicle.car();
         } else if (typeVehicle == TypeVehicle.MOTORCYCLE){
             Vehicle vehicle = new Vehicle("white", "2010", 250);
             vehicle.motorcycle();
         }
-		
-	}
-
+    }
 }
